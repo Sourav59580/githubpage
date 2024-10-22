4ada4b7c396529a0e0a16f24beed7b66012cf5b9
soura1121 <sourav.1121@zenmonk.tech>
2024-10-22 20:33:23 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Define your components for each route
+function Home() {
+  return <h1>Hello</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function Careers() {
+  return <h1>Careers</h1>;
+}
+
 function App() {
   return (
-   <>cdcd</>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/careers" element={<Careers />} />
+      </Routes>
+    </Router>
   );
 }
 
